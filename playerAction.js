1ae31582432da110eed5e820dd8bfa691b341c1b
iwanaboz <64489989+iwanaboz@users.noreply.github.com>
2020-05-01 02:50:48 +0900

    Add files via upload

--- playerAction.js
+++ playerAction.js
@@ -2,10 +2,10 @@
 function userMove(frameTime, mesh, agent) {
 	
 	//　マウス位置で方向転換
-	canvasWidth = renderer.domElement.offsetWidth;
-	canvasHeight = renderer.domElement.offsetHeight;
-	dest_angleRight = Math.atan2(mousex, canvasWidth);
-	dest_angleUp 	= Math.atan2(mousey, canvasHeight);
+	var canvasWidth = renderer.domElement.offsetWidth;
+	var canvasHeight = renderer.domElement.offsetHeight;
+	var dest_angleRight = Math.atan2(mousex, canvasWidth);
+	var dest_angleUp 	= Math.atan2(mousey, canvasHeight);
 	// 水平方向（マウスを押しているとき）
 	if( dest_angleRight && mouseDrag>0){
 		player.rotationRight += frameTime * dest_angleRight *5;
@@ -24,10 +24,14 @@ function userMove(frameTime, mesh, agent) {
 	if(key_on[16]>0){
 		speed = 24;
 	}
+	
+	var dest_lookRight;
 	//w (前進)
 	if(key_on[87]>0){
 		agent.position.z += frameTime * speed * agent.viewVect.z;
 		agent.position.x += frameTime * speed * agent.viewVect.x;
+		dest_lookRight = agent.rotationRight;
+		
 		if(key_on[16]>0){ isMove_ = 2;}else{isMove_ = 1;}
 		
 	}
@@ -35,20 +39,43 @@ function userMove(frameTime, mesh, agent) {
 	if(key_on[65]>0){
 		agent.position.z -= frameTime * speed * agent.viewVect.x;
 		agent.position.x += frameTime * speed * agent.viewVect.z;
+		dest_lookRight = agent.rotationRight+THREE.Math.degToRad( -90 );
+		
 		if(key_on[16]>0){ isMove_ = 2;}else{isMove_ = 1;}
 	}
 	//s (後退)
 	if(key_on[83]>0){
 		agent.position.z -= frameTime * speed * agent.viewVect.z;
 		agent.position.x -= frameTime * speed * agent.viewVect.x;
+		dest_lookRight = agent.rotationRight+THREE.Math.degToRad( -180 );
+		
 		if(key_on[16]>0){ isMove_ = 2;}else{isMove_ = 1;}
 	}
 	//d (右)
 	if(key_on[68]>0){
 		agent.position.z += frameTime * 5 * agent.viewVect.x;
 		agent.position.x -= frameTime * 5 * agent.viewVect.z;
+		dest_lookRight = agent.rotationRight+THREE.Math.degToRad( +90 );
+		
 		if(key_on[16]>0){ isMove_ = 2;}else{isMove_ = 1;}
 	}
+	
+	if(isMove_ > 0){
+		dest_lookRight = dest_lookRight%THREE.Math.degToRad(360);
+		agent.lookingRight = agent.lookingRight%THREE.Math.degToRad(360);
+		
+		var diff_angle = dest_lookRight-agent.lookingRight;
+		if(  Math.abs(diff_angle+THREE.Math.degToRad(360)) < Math.abs(diff_angle) ){
+			diff_angle +=THREE.Math.degToRad(360);
+		}
+		if(  Math.abs(diff_angle-THREE.Math.degToRad(360)) < Math.abs(diff_angle) ){
+			diff_angle -=THREE.Math.degToRad(360);
+		}
+		//if(Math.abs(diff_angle)<THREE.Math.degToRad( 180 )){
+			agent.lookingRight += diff_angle*frameTime*4;
+
+	}
+	
 	if(player.isStop==1 && isMove_ > 0){
 		player.isStop=0;
 	}else if(player.isStop==0 && isMove_ == 0){
@@ -60,7 +87,7 @@ function userMove(frameTime, mesh, agent) {
 	if(isMove_==2){selectmotion = 1;}
 	
 	// 反映する
-	mesh.rotation.y = -agent.rotationRight;
+	mesh.rotation.y = -agent.lookingRight;
 	mesh.position.x = agent.position.x;
 	mesh.position.y = agent.position.y;
 	mesh.position.z = agent.position.z;
