74e65edf1ad29217492130e6b5ebaf03049bea30
iwanaboz <64489989+iwanaboz@users.noreply.github.com>
2020-05-08 01:03:01 +0900

    test
    test

--- scripts/v10/MainLoop.js
+++ scripts/v10/MainLoop.js
@@ -4,7 +4,7 @@ function mainLoop(frameTime) {
 	
 	if(player.chara._isLoaded>1){
 		// playerAction.js
-		userMove(frameTime, player);
+		//userMove(frameTime, player);
 		updateAction(frameTime, player);
 		cameraUpDate(player);
 		
