d2eebb3a03d6566889b84d3f1dd05517b31a501b
kawaharaVision <poiristadt@gmail.com>
2021-11-20 13:06:28 +0900

    Update SSCommon.js

--- shikihime_shooting/SSCommon.js
+++ shikihime_shooting/SSCommon.js
@@ -90,7 +90,7 @@ function directionCheck() {
     var viewportContent;
     if(direction == 90 || direction == -90 ){
 		//yoko
-		viewportContent = "width=device-width, initial-scale=0.6,user-scalable=no";
+		viewportContent = "width=device-width, initial-scale=0.5,user-scalable=no";
         console.log('90');
     } else {
     	// default
